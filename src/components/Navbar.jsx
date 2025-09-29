2c82b72a0da2f2605fc9fe17705219ba58e7c807
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-09-28 23:54:36 -0700

    completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src='../assets/react.svg' width={40}></img>
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
