903ddf2235b29eb0762c709ef769d97dd768707b
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-09-29 00:06:56 -0700

    changed img src

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src='../assets/react.svg' width={40}></img>
+                <img src='/assets/react.svg' width={40}></img>
                 <span>ReactFacts</span>
             </nav>
         </header>
